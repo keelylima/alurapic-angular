2f86ade4cea5c2793be181b518600941e9f40baa
Kelly Lima <keely06@live.com>
2020-09-01 15:32:37 -0300

    add httpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'alurapic';
+  photos = [];
+
+  constructor(http: HttpClient) {
+    console.log(http);
+  }
 }

0de3a80923884a7b097e12de7662cfcf12e266a1
Kelly Lima <keely06@live.com>
2020-09-08 12:14:56 -0300

    change li to row with 3 photos. Used ngOnChanges

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Photo } from '../../photo/photo';
 
 @Component({
@@ -6,11 +6,24 @@ import { Photo } from '../../photo/photo';
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.css'],
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnChanges {
   @Input() photosDoPhotos: Photo[] = [];
   rows: any[] = [];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.photosDoPhotos) {
+      this.rows = this.groupColumns(this.photosDoPhotos);
+    }
+  }
+
+  groupColumns(photos: Photo[]) {
+    const newRows = [];
+
+    for (let index = 0; index < photos.length; index += 3) {
+      newRows.push(photos.slice(index, index + 3));
+    }
+    return newRows;
+  }
 }

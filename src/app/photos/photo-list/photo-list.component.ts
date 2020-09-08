ca9e440277058779dbed0a5202e0cc454e706482
Kelly Lima <keely06@live.com>
2020-09-08 17:13:06 -0300

    add debounce and ngOnDestroy

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,19 +1,32 @@
-import { Photo } from './../photo/photo';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+import { Photo } from './../photo/photo';
 
 @Component({
   selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css'],
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
   photosBack: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.photosBack = this.activatedRoute.snapshot.data.photos;
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe((filter) => (this.filter = filter));
+  }
+  ngOnDestroy(): void {
+    //trabalhando com subscribe/rxjs
+    //nunca chega no debounce.complete()
+    //ele fica ocupando espaço na memoria mesmo indo pra outras áreas, usar o unsubscribe para destruir isso
+    this.debounce.unsubscribe();
   }
 }

d5b566dfcacb0735c93bdcd1bf790937ec18718c
Kelly Lima <keely06@live.com>
2020-09-11 15:06:14 -0300

    create auth/authenticate

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
   templateUrl: './signin.component.html',
@@ -7,12 +8,29 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class SignInComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
       userName: ['', Validators.required],
       password: ['', Validators.required],
     });
+  }
+
+  login() {
+    const usernName = this.loginForm.get('userName').value;
+    const password = this.loginForm.get('password').value;
+
+    this.authService.authenticate(usernName, password).subscribe(
+      () => console.log('autenticado'),
+      (err) => {
+        console.log(err);
+        this.loginForm.reset();
+        alert('Invalid user name or password');
+      }
+    );
   }
 }

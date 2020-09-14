d0291e0a9b4087fd20e6ca4abd32cf32427cd989
Kelly Lima <keely06@live.com>
2020-09-14 20:08:31 -0300

    add focus do input/used viewChild

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/core/auth.service';
@@ -8,6 +8,7 @@ import { AuthService } from 'src/app/core/auth.service';
 })
 export class SignInComponent implements OnInit {
   loginForm: FormGroup;
+  @ViewChild('userNameInput') userNameInput: ElementRef<HTMLInputElement>;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -31,6 +32,7 @@ export class SignInComponent implements OnInit {
       (err) => {
         console.log(err);
         this.loginForm.reset();
+        this.userNameInput.nativeElement.focus();
         alert('Invalid user name or password');
       }
     );

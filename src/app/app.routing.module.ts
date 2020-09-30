b7c939e055a5eee4c06dda749911eea7ef4f08a6
Kelly Lima <keely06@live.com>
2020-09-29 21:27:50 -0300

    add html sign up

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,3 +1,4 @@
+import { SignupComponent } from './home/signup/signup.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -13,6 +14,10 @@ const routes: Routes = [
     path: '',
     component: SignInComponent,
     canActivate: [AuthGuard],
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
   },
   {
     path: 'user/:user',
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/signin.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { VmessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [SignInComponent],
-  imports: [CommonModule, ReactiveFormsModule, VmessageModule, RouterModule],
+  declarations: [SignInComponent, SignupComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    VmessageModule,
+    RouterModule,
+  ],
 })
 export class HomeModule {}

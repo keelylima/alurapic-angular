b98ed754648bc555ac64966b4873aaee5797e6c9
Kelly Lima <keely06@live.com>
2020-10-10 16:18:36 -0300

    add html photo-form

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { PhotoFormComponent } from './photo-form.component';
+import { VmessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 
 @NgModule({
   declarations: [PhotoFormComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, VmessageModule, FormsModule],
 })
 export class PhotoFormModule {}
